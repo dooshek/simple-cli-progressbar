4e2a59573b03b46d20d0d126bc71f91fcf7689bf
Piotr Duszynski <dooshek@desmart.com>
2020-05-24 20:00:35 +0200

    ADD: examples

--- examples/3.js
+++ examples/3.js
@@ -1,10 +1,18 @@
 const SimpleProgressBar = require('../index')
+const chalk = require('chalk')
 const delay = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
 const test = async function () {
   // How many records in total? 
-  const totalRecords = 250
-  const progressbar = new SimpleProgressBar({ total: totalRecords })
+  const totalRecords = 500
+  const progressbar = new SimpleProgressBar({ 
+    total: totalRecords,
+    completed: '▲',
+    pending: '▽',
+    colors: {
+      pending: chalk.gray
+    }
+  })
 
   for (let i = 0; i < totalRecords; i++) {
 
